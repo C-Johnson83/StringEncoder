f03187e9c956276a9afbe5c31fc56fef8734dc8a
Chris Johnson <Chrisj21283@gmail.com>
2026-07-19 09:10:42 -0400

    Modify QualityHubLauncher.bat to use relative paths
    Updated file paths in QualityHubLauncher.bat for better relative referencing.

--- QualityHubLauncher.bat
+++ QualityHubLauncher.bat
@@ -16,8 +16,8 @@ echo.
 
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe ^
 -NoProfile -NonInteractive -NoLogo -ExecutionPolicy Bypass ^
--File "J:\zz_Access_Tools_Data\Developers\OpenWorkbook.ps1" ^
--PwFilePath "J:\zz_Access_Tools_Data\Developers\.sysconfig\qh_42a.dat" ^
--WorkbookPath "J:\zz_Access_Tools_Data\Developers\encryptedWorkbook.xlsm"
+-File "..\..\OpenWorkbook.ps1" ^
+-PwFilePath "..\..\ created.dat" ^
+-WorkbookPath "..\..\encryptedWorkbook.xlsm"
 
 exit
