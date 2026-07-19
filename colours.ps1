386f23fbb3260b3570eac6309895f8d8de45516b
Chris Johnson <Chrisj21283@gmail.com>
2026-07-19 09:21:50 -0400

    Enhance colours.ps1 with execution instructions and pause
    Added instructions for running the script with execution bypass and included a pause at the end.

--- colours.ps1
+++ colours.ps1
@@ -1,5 +1,9 @@
 # output all the colour combinations for text/background
 # https://stackoverflow.com/questions/20541456/list-of-all-colors-available-for-powershell/41954792#41954792
+
+# run in the terminal using execution bypass
+# powershell -ExecutionPolicy Bypass -File .\colours.ps1
+
 $colors = [enum]::GetValues([System.ConsoleColor])
 Foreach ($bgcolor in $colors) {
     Foreach ($fgcolor in $colors) { Write-Host "$fgcolor|" -ForegroundColor $fgcolor -BackgroundColor $bgcolor -NoNewLine }
